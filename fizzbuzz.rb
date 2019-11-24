5ac3c03fc5217c77224962d8b77f3cdc7dff8b35
weng lin <wenglin@buffalo.edu>
2019-11-24 07:50:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz
   if int%3 == 0
-    "Fizz"
+    return "Fizz"
   end
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
